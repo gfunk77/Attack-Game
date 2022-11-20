726d30f24cf49bc1af006f510c0890c5ed969915
gfunk77 <faraci77@gmail.com>
2022-11-20 14:49:43 -0500

    implement game log

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
       playerHealth: 100,
       attackCounter: 0,
       winner: null,
+      gameStats: [],
     };
   },
   watch: {
@@ -48,27 +49,52 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startNewGame() {
+      this.alienHealth = 100;
+      this.playerHealth = 100;
+      this.attackCounter = 0;
+      this.winner = null;
+      this.gameStats = [];
+    },
     attackAlien() {
       this.attackCounter++;
       const attackValue = getRandomNumber(5, 10);
       this.alienHealth -= attackValue;
+      this.gameLog("player", "attack", attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomNumber(8, 13);
       this.playerHealth -= attackValue;
+      this.gameLog("alien", "attack", attackValue);
     },
     specialAttack() {
       this.attackCounter++;
       const attackValue = getRandomNumber(8, 20);
       this.alienHealth -= attackValue;
+      this.gameLog("player", "special attack", attackValue);
       this.attackPlayer();
     },
     recover() {
       this.attackCounter++;
       const recoverValue = getRandomNumber(12, 22);
-      this.playerHealth += recoverValue;
+      if (this.playerHealth + recoverValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += recoverValue;
+      }
+      this.gameLog("player", "recover", recoverValue);
       this.attackPlayer();
+    },
+    abduction() {
+      this.winner = "alien";
+    },
+    gameLog(who, what, type) {
+      this.gameStats.unshift({
+        who: who,
+        action: what,
+        type: type,
+      });
     },
   },
 });
